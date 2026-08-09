c4506a483a0e683d2186051fe3cfaeda07723e16
LAVKUSH YADAV <lavkush2744923yadav@gmail.com>
2026-08-09 17:35:09 +0530

    2nd version of next greater element

---
+++ Stack/nextGreaterEl1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr1={4,1,2};
+    int n1=arr1.size();
+    vector<int>arr2={1,3,4,2};
+    int n2=arr2.size();
+    vector<int>ans(n1);
+    stack<int>s;
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n2;j++){
+            if(arr1[i]==arr2[j]){
+                if(j==n2-1){
+                    ans[i]=-1;
+                }
+                for(int k=j+1;k<n2;k++){
+                       
+                       if(arr2[j]<arr2[k]){
+                           ans[i]=arr2[k];
+                           break;
+                        }
+                        if(k==n2-1)
+                           ans[i]=-1;                 
+                }
+            }
+        }
+    }
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+}
