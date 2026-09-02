f06eaecce73a0e40acb5d620ba17cb6a837f9a7b
LAVKUSH YADAV <lavkush2744923yadav@gmail.com>
2026-09-02 18:45:43 +0530

    Resolve

--- Linked List/reverseLinkedList.cpp
+++ Linked List/reverseLinkedList.cpp
@@ -9,23 +9,25 @@ class Node{
         next=NULL;
     }
 };
-void reverse(Node* head){
+
+Node* reverse(Node* head){
     Node* prev=NULL;
-    Node* curr=NULL;
-    Node* nextNode=NULL;
+    Node* curr=head;
     while(curr!=NULL){
-        nextNode=curr->next;
+        Node* nextNode= curr->next;
         curr->next=prev;
         prev=curr;
         curr=nextNode;
-        cout<<prev->data<<" -> ";
     }
-    
+    return prev;
 }
 int main(){
   Node *head=new Node(5);
   head->next=new Node(9);
   head->next->next=new Node(8);
   head->next->next->next=new Node(2);
-  reverse(head);
+  Node* root=reverse(head);
+
+  cout<<"The Head of the reverse Linked List : "<<root->data;
+  
 }
